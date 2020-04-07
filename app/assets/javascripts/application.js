aff45f02511ec13b26140a3b7cc4c17b9a849153
Scott Bobsin <scottbobsin@gmail.com>
2020-04-07 15:02:03 -0700

    jumbotron and navigation

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,6 +11,12 @@
 // about supported directives.
 //
 //= require rails-ujs
+//= require jquery3
+//= require popper
+//= require bootstrap-sprockets
 //= require activestorage
 //= require turbolinks
 //= require_tree .
+
+
+
